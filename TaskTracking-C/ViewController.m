135577d139df47332f94fa031bbda64c5f1f293c
耿瑞 <gengr.r@gmail.com>
2016-12-08 20:24:13 -0500

    Initial Commit

---
+++ TaskTracking-C/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TaskTracking-C
+//
+//  Created by GengRui on 2016-12-08.
+//  Copyright © 2016 GengRui. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ TaskTracking-C/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TaskTracking-C
+//
+//  Created by GengRui on 2016-12-08.
+//  Copyright © 2016 GengRui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
